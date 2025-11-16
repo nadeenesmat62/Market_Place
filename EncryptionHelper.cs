599968e5c1d9aac74b6560c08c7ca4ea36d170d6
nadeenesmat62 <nadeenesmat62@gmail.com>
2025-11-16 17:01:35 +0200

    Helper
    Helper

---
+++ EncryptionHelper.cs
@@ -0,0 +1,69 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using JWTRefreshTokenInDotNet6.Models;
+using System.IO.Compression;
+
+namespace JWTRefreshTokenInDotNet6.Helpers
+{
+    public class EncryptionHelper
+    {
+        private string _key;
+        private string _iv;
+
+        public void Initialize(IConfiguration configuration)
+        {
+            _key = configuration["EncryptionSettings:Key"];
+            _iv = configuration["EncryptionSettings:IV"];
+        }
+
+        public string Encrypt(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(_key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(_iv);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(_key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(_iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
